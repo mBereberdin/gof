5e2bc875f2aab4bc642f5a9cc31a9a7a18cc28b0
Mikhail Bereberdin <bereberdin999@icloud.com>
2023-09-16 00:34:42 +0700

    Добавление логирования

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -1,42 +1,37 @@
-using System.Reflection;
-
 using Example.Extensions;
 
 using gof.Creational.AbstractFactory.Extensions;
 
-using Microsoft.OpenApi.Models;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Gof example api",
-        Description = "ASP.NET Core Web API для проверки работы gof."
-    });
+using var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
 
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
-        xmlFilename));
-});
+var isUseSwagger = builder.Configuration.GetValue<bool>("IsUseSwagger");
+if (isUseSwagger)
+{
+    builder.Services.AddSwaggerGeneration(logger);
+}
 
 builder.Services.AddAbstractFactory();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-var isUseSwagger = app.Configuration.GetValue<bool>("IsUseSwagger");
 
 app.UseStaticFiles();
 
 if (isUseSwagger)
 {
-    app.AddAndConfigureSwagger();
+    app.AddAndConfigureSwagger(logger);
 }
 
 app.MapControllers();
+
+Log.CloseAndFlush();
 
 app.Run();
 
